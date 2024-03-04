5ea5cb07df1060ea198815ee901b74f289555169
uptech-wook <wook@uptech.partners>
2024-03-04 18:47:34 +0900

    login added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import FacebookLogin from '@greatsumini/react-facebook-login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>hihihi</div>
+      <FacebookLogin
+        appId="1464420907827345"
+        onSuccess={(response) => {
+          console.log('Login Success!', response);
+        }}
+        onFail={(error) => {
+          console.log('Login Failed!', error);
+        }}
+        onProfileSuccess={(response) => {
+          console.log('Get Profile Success!', response);
+        }}
+      />
     </div>
   );
 }
