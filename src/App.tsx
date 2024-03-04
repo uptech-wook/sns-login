e976d5ef1d5be2c761936e846d00b43c448ef693
uptech-wook <wook@uptech.partners>
2024-03-04 18:44:34 +0900

    error update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import FacebookLogin from '@greatsumini/react-facebook-login';
 
 function App() {
   return (
-    <div className="App"> 
-      <div>FACEBOOK LOGIN</div>
-      <FacebookLogin
-        appId='1464420907827345'
-
-      />
+    <div className="App">
+      <div>hihihi</div>
     </div>
   );
 }
