1eb734fe2354f288c5431f0a91e1a6e8d269de0d
uptech-wook <wook@uptech.partners>
2024-03-04 18:09:35 +0900

    페이스북 로그인 텍스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import FacebookLogin from '@greatsumini/react-facebook-login';
 function App() {
   return (
     <div className="App"> 
+      <div>FACEBOOK LOGIN</div>
       <FacebookLogin
         appId='1464420907827345'
-        
+
       />
     </div>
   );
